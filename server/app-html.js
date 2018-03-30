8dc977d18ef4d9003cfe371e190f496d73e39538
Pavel Ramasheuski <rompasha2017@gmail.com>
2018-03-30 23:17:01 +0300

    express, http, error page

---
+++ server/app-html.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require("fs");
+
+http.createServer((req, res) => {
+    const url = req.url === '/' ? '/index.html' : req.url;
+
+    fs.readFile('../public' + url, (err, data) => {
+        if (!err) {
+            res.end(data);
+        }
+        else {
+            fs.readFile('../public/error.html', (err, data) => {
+                if (!err) {
+                    res.end(data);
+                }
+                else {
+                    res.end(err.message);
+                }
+            });
+        }
+    });
+
+}).listen(3000);
